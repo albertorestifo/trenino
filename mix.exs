85c271abf03915d7202fff1a36529c4c6a1e7d5e
Alberto Restifo <alberto@restifo.dev>
2025-12-20 22:09:49 +0100

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TswIo.MixProject do
   def project do
     [
       app: :tsw_io,
-      version: "0.1.4",
+      version: "0.2.0",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
