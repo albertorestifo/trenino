eabfcc8afb771d48febf92a1e2b900f66573790b
Alberto Restifo <alberto@restifo.dev>
2026-08-10 01:25:18 +0200

    test: satisfy migration isolation quality checks

--- test/ci/migration_module_isolation_test.exs
+++ test/ci/migration_module_isolation_test.exs
@@ -1,13 +1,15 @@
 defmodule Trenino.CI.MigrationModuleIsolationTest do
   use ExUnit.Case, async: false
 
+  alias Trenino.Test.MigrationModuleIsolation
+
   test "purges migration modules loaded by the Mix test alias" do
     module = Trenino.Repo.Migrations.WarningRegressionFixture
 
     Module.create(module, quote(do: def(change, do: :ok)), Macro.Env.location(__ENV__))
     assert Code.ensure_loaded?(module)
 
-    purged = Trenino.Test.MigrationModuleIsolation.purge_loaded()
+    purged = MigrationModuleIsolation.purge_loaded()
 
     assert module in purged
     assert Enum.all?(purged, &(Code.ensure_loaded?(&1) == false))
